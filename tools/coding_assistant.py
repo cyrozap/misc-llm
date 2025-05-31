cyrozap/misc-llm

commit bda083b9a0b21ba20e02d0a4e3bc4fd7088d16bd

Add support for Devstral

tools/coding_assistant.py
@@ -31,6 +31,7 @@ from bs4.element import PageElement, Tag
 
 MODELS: list[str] = [
     "qwen2.5-coder:32b-instruct-q4_K_M-17k",
+    "devstral:24b-small-2505-q4_K_M-61k",
     "qwen3:14b-q4_K_M-95k",
     "qwen3:30b-a3b-q4_K_M-46k",
     "qwen3:32b-q4_K_M-12k",

modelfiles/devstral:24b-small-2505-q4_K_M-61k.modelfile
@@ -1,0 +1,2 @@
+FROM devstral:24b-small-2505-q4_K_M
+PARAMETER num_ctx 62464

commit ca9a3aec4771afe2aad74644c7f6896e50a30fe6

Remove Devstral from the list of models in coding_assistant.py
Fixes: bda083b ("Add support for Devstral")

tools/coding_assistant.py
@@ -31,7 +31,6 @@ from bs4.element import PageElement, Tag
 
 MODELS: list[str] = [
     "qwen2.5-coder:32b-instruct-q4_K_M-17k",
-    "devstral:24b-small-2505-q4_K_M-61k",
     "qwen3:14b-q4_K_M-95k",
     "qwen3:30b-a3b-q4_K_M-46k",
     "qwen3:32b-q4_K_M-12k",