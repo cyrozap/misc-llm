cyrozap/misc-llm

commit 8a8040e5815675ab16d43b9d15fa6ee67833b908

Switch to updated Qwen3 30B A3B models

tools/translate.py
@@ -26,7 +26,7 @@ import openai
 
 
 MODELS: list[str] = [
-    "qwen3:30b-a3b-q4_K_M-40k",
+    "qwen3:30b-a3b-instruct-2507-q4_K_M-47k",
     "qwen3:14b-q4_K_M-40k",
     "qwen2.5-coder:32b-instruct-q4_K_M-17k",
     "granite3.3:8b-128k",

modelfiles/qwen3:30b-a3b-instruct-2507-q4_K_M-47k.modelfile
@@ -1,0 +1,2 @@
+FROM qwen3:30b-a3b-instruct-2507-q4_K_M
+PARAMETER num_ctx 48128

modelfiles/qwen3:30b-a3b-thinking-2507-q4_K_M-47k.modelfile
@@ -1,0 +1,2 @@
+FROM qwen3:30b-a3b-thinking-2507-q4_K_M
+PARAMETER num_ctx 48128

tools/coding_assistant.py
@@ -35,7 +35,7 @@ MODELS: list[str] = [
     "qwen2.5-coder:32b-instruct-q4_K_M-17k",
 
     "qwen3:14b-q4_K_M-40k",
-    "qwen3:30b-a3b-q4_K_M-40k",
+    "qwen3:30b-a3b-thinking-2507-q4_K_M-47k",
     "qwen3:32b-q4_K_M-12k",
 
     "deepseek-r1:8b-0528-qwen3-q4_K_M-128k",

commit a2927fc5dd4de9091ec02b284a0f412cba2e2e80

Only append /no_think to the prompt for the thinking versions of Qwen3
Fixes: 8a8040e ("Switch to updated Qwen3 30B A3B models")

tools/translate.py
@@ -80,7 +80,7 @@ def main() -> int:
 
     system_message: str = f"Translate the provided text to {args.language}. YOU MUST ONLY OUTPUT THE TRANSLATED TEXT!"
 
-    if args.model.startswith("qwen3:"):
+    if args.model.startswith("qwen3:") and not args.model.startswith("qwen3:30b-a3b-instruct-2507"):
         system_message += " /no_think"
 
     messages: list[dict[str, str]] = [