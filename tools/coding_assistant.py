cyrozap/misc-llm

commit 8ebacbf43fb25fe715306cc46c873be979588394

Add support for Qwen 3.5

tools/coding_assistant.py
@@ -36,6 +36,9 @@ MODELS: list[str] = [
 
     "qwen3-coder:30b-a3b-q4_K_M-200k",
 
+    "qwen3.5:27b-q4_K_M-160k"
+    "qwen3.5:35b-a3b-q4_K_M-200k",
+
     "trinity-mini:26b-a3b-instruct-2512-q4_K_M-128k",
 
     "gpt-oss:120b-46k",

modelfiles/5090/qwen3.5:27b-q4_K_M-160k.modelfile
@@ -1,0 +1,2 @@
+FROM qwen3.5:27b-q4_K_M
+PARAMETER num_ctx 163840

modelfiles/5090/qwen3.5:35b-a3b-q4_K_M-200k.modelfile
@@ -1,0 +1,2 @@
+FROM qwen3.5:35b-a3b-q4_K_M
+PARAMETER num_ctx 204800

commit 4f407aa5b30cd5247c6b019ec7819f8473cbb924

Add missing comma
Fixes: 8ebacbf ("Add support for Qwen 3.5")

tools/coding_assistant.py
@@ -36,7 +36,7 @@ MODELS: list[str] = [
 
     "qwen3-coder:30b-a3b-q4_K_M-200k",
 
-    "qwen3.5:27b-q4_K_M-160k"
+    "qwen3.5:27b-q4_K_M-160k",
     "qwen3.5:35b-a3b-q4_K_M-200k",
 
     "trinity-mini:26b-a3b-instruct-2512-q4_K_M-128k",